eed77c5eb2c734cdaf29f36f4eb60dbc086e3f5b
knk <knk@knknoMacBook-puro.local>
2018-10-09 18:03:40 +0900

    フォローフォロワー

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#index'
+
+  resources :users do
+    member do
+     get :following, :followers
+    end
+  end
+  resources :relationships,only: [:create, :destroy]
+end
